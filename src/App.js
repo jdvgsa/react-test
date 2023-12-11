e942e11ffc2afa135a99d2bf07d7aee85be5ac48
jdvgsa <johnr.dagsa@outlook.com>
2023-12-11 10:13:25 -0700

    created react button and some styling for H1, and styling for the button hover effects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const handleClick = () => { 
+    alert('Button clicked!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="h1">This is my Cool Button that I made with React!</h1>
+        
+        <button className="button" onClick={handleClick}>Click me!!</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
